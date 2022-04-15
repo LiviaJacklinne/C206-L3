151fddc46e8b6f7a6f9d239e7e4fc3b7d18f26a9
LiviaJacklinne <livia.jack02@gmail.com>
2022-04-15 10:42:03 -0300

    Prova 02

--- Prova-02/src/main/java/Main.java
+++ Prova-02/src/main/java/Main.java
@@ -2,15 +2,14 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Personagem personagens[] = new Personagem[4];
+        Personagem personagens1[] = new Personagem[2];
+        Personagem personagens2[] = new Personagem[2];
 
         Guilda guilda1 = new Guilda();
         guilda1.setNome("Humanos e Anãos");
-        guilda1.setMembros(personagens);
 
         Guilda guilda2 = new Guilda();
         guilda2.setNome("Trolls e Orcs");
-        guilda2.setMembros(personagens);
 
         Anao anao1 = new Anao("Medio");
         anao1.setNome("Anao 1");
@@ -32,18 +31,23 @@ public class Main
         troll1.setVida(45);
         troll1.setClasse("Xama");
 
-        personagens[0] = anao1;
-        personagens[1] = humano1;
-        personagens[2] = orc1;
-        personagens[3] = troll1;
+        personagens1[0] = anao1;
+        personagens1[1] = humano1;
+        guilda1.setMembros(personagens1);
 
-        for(int i=0; i< personagens.length;i++)
+        personagens2[0] = orc1;
+        personagens2[1] = troll1;
+        guilda2.setMembros(personagens2);
+
+
+
+        for(int i=0; i< personagens1.length;i++)
         {
-            if(personagens[i] != null)
+            if(personagens1[i] != null)
             {
-                if(personagens[i] instanceof Anao)
+                if(personagens1[i] instanceof Anao)
                 {
-                    Anao a = (Anao) personagens[i];
+                    Anao a = (Anao) personagens1[i];
                     a.mostraInfo();
                     a.endurecer();
                     a.CastarSpell();
@@ -53,9 +57,9 @@ public class Main
                     a.entrarEmRaid();
 
                 }
-                else if(personagens[i] instanceof Humano)
+                else if(personagens1[i] instanceof Humano)
                 {
-                    Humano h = (Humano) personagens[i];
+                    Humano h = (Humano) personagens1[i];
                     h.mostraInfo();
                     h.motivar();
                     h.ForTheAllience();
@@ -65,9 +69,9 @@ public class Main
                     h.entrarEmRaid();
 
                 }
-                else if(personagens[i] instanceof Orc)
+                else if(personagens1[i] instanceof Orc)
                 {
-                    Orc o = (Orc) personagens[i];
+                    Orc o = (Orc) personagens1[i];
                     o.mostraInfo();
                     o.enfurecer();
                     o.sacarArma();
@@ -78,7 +82,7 @@ public class Main
                 }
                 else // TROLL
                 {
-                    Troll t = (Troll) personagens[i];
+                    Troll t = (Troll) personagens1[i];
                     t.mostraInfo();
                     t.regenerar();
                     t.ForTheHorde();
@@ -92,6 +96,60 @@ public class Main
             }
         }
 
+        for(int i=0; i< personagens2.length;i++)
+        {
+            if(personagens2[i] != null)
+            {
+                if(personagens2[i] instanceof Anao)
+                {
+                    Anao a = (Anao) personagens2[i];
+                    a.mostraInfo();
+                    a.endurecer();
+                    a.CastarSpell();
+                    a.ForTheAllience();
+                    a.InvadirHorda();
+                    a.entrarEmPvP();
+                    a.entrarEmRaid();
+
+                }
+                else if(personagens2[i] instanceof Humano)
+                {
+                    Humano h = (Humano) personagens2[i];
+                    h.mostraInfo();
+                    h.motivar();
+                    h.ForTheAllience();
+                    h.InvadirHorda();
+                    h.sacarArma();
+                    h.entrarEmPvP();
+                    h.entrarEmRaid();
+
+                }
+                else if(personagens2[i] instanceof Orc)
+                {
+                    Orc o = (Orc) personagens2[i];
+                    o.mostraInfo();
+                    o.enfurecer();
+                    o.sacarArma();
+                    o.ForTheHorde();
+                    o.InvadirAlianca();
+                    o.entrarEmPvP();
+                    o.entrarEmRaid();
+                }
+                else // TROLL
+                {
+                    Troll t = (Troll) personagens2[i];
+                    t.mostraInfo();
+                    t.regenerar();
+                    t.ForTheHorde();
+                    t.InvadirAlianca();
+                    t.CastarSpell();
+                    t.entrarEmPvP();
+                    t.entrarEmRaid();
+
+                }
+
+            }
+        }
         guilda1.mostraInfo();
         guilda2.mostraInfo();
 
